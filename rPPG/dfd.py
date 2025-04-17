be0a0f1219861eb77bee2165a6a6537d45ad48c9
KrrishK04 <128284710+KrrishK04@users.noreply.github.com>
2025-04-17 17:00:20 +0530

    Update dfd.py

--- rPPG/dfd.py
+++ rPPG/dfd.py
@@ -781,16 +781,16 @@ def run_deepfake_detection(
 if __name__ == "__main__":
     
     #real test videos
-    VIDEO_PATH = 'test/real/1'
-    # VIDEO_PATH = 'test/real/2'
-    # VIDEO_PATH = 'test/real/3'
-    # VIDEO_PATH = 'test/real/4'
+    VIDEO_PATH = 'test/real/1.mp4'
+    # VIDEO_PATH = 'test/real/2.mp4'
+    # VIDEO_PATH = 'test/real/3.mp4'
+    # VIDEO_PATH = 'test/real/4.mp4'
     
     #fake test videos
-    # VIDEO_PATH = 'test/fake/1'
-    # VIDEO_PATH = 'test/fake/2'
-    # VIDEO_PATH = 'test/fake/3'
-    # VIDEO_PATH = 'test/fake/4'
+    # VIDEO_PATH = 'test/fake/1.mp4'
+    # VIDEO_PATH = 'test/fake/2.mp4'
+    # VIDEO_PATH = 'test/fake/3.mp4'
+    # VIDEO_PATH = 'test/fake/4.mp4'
     
     #Also You can set the video path to none to use laptop camera to check for deepfake, for that you might need to configure the camera id.
     #VIDEO_PATH = ''
